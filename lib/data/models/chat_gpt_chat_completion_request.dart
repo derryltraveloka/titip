e0185c8fe04173d19656e49ad68c04f65a281181
Derryl <derryl.taufik@traveloka.com>
2023-07-06 18:50:34 +0700

    added chat completion request & response

--- lib/data/models/chat_gpt_chat_completion_request.dart
+++ lib/data/models/chat_gpt_chat_completion_request.dart
@@ -1,3 +1,45 @@
-class ChatGptChatCompletionRequest{
+import 'dart:convert';
 
+class ChatGptChatCompletionRequest {
+  final List<Message>? messages;
+
+  ChatGptChatCompletionRequest({
+    this.messages,
+  });
+
+  factory ChatGptChatCompletionRequest.fromRawJson(String str) => ChatGptChatCompletionRequest.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ChatGptChatCompletionRequest.fromJson(Map<String, dynamic> json) => ChatGptChatCompletionRequest(
+    messages: json["messages"] == null ? [] : List<Message>.from(json["messages"]!.map((x) => Message.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "messages": messages == null ? [] : List<dynamic>.from(messages!.map((x) => x.toJson())),
+  };
+}
+
+class Message {
+  final String? role;
+  final String? content;
+
+  Message({
+    this.role,
+    this.content,
+  });
+
+  factory Message.fromRawJson(String str) => Message.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Message.fromJson(Map<String, dynamic> json) => Message(
+    role: json["role"],
+    content: json["content"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "role": role,
+    "content": content,
+  };
 }
--- lib/data/models/chat_gpt_chat_completion_response.dart
+++ lib/data/models/chat_gpt_chat_completion_response.dart
@@ -1,3 +1,55 @@
-class ChatGptChatCompletionResponse{
+// To parse this JSON data, do
 
+import 'dart:convert';
+
+class ChatGptChatCompletionResponse {
+  final String? message;
+  final List<RecommendedHotel>? recommendedHotels;
+  final String? trailingMessage;
+
+  ChatGptChatCompletionResponse({
+    this.message,
+    this.recommendedHotels,
+    this.trailingMessage,
+  });
+
+  factory ChatGptChatCompletionResponse.fromRawJson(String str) => ChatGptChatCompletionResponse.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ChatGptChatCompletionResponse.fromJson(Map<String, dynamic> json) => ChatGptChatCompletionResponse(
+    message: json["message"],
+    recommendedHotels: json["recommendedHotels"] == null ? [] : List<RecommendedHotel>.from(json["recommendedHotels"]!.map((x) => RecommendedHotel.fromJson(x))),
+    trailingMessage: json["trailingMessage"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "recommendedHotels": recommendedHotels == null ? [] : List<dynamic>.from(recommendedHotels!.map((x) => x.toJson())),
+    "trailingMessage": trailingMessage,
+  };
+}
+
+class RecommendedHotel {
+  final String? city;
+  final List<String>? hotels;
+
+  RecommendedHotel({
+    this.city,
+    this.hotels,
+  });
+
+  factory RecommendedHotel.fromRawJson(String str) => RecommendedHotel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory RecommendedHotel.fromJson(Map<String, dynamic> json) => RecommendedHotel(
+    city: json["city"],
+    hotels: json["hotels"] == null ? [] : List<String>.from(json["hotels"]!.map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "city": city,
+    "hotels": hotels == null ? [] : List<dynamic>.from(hotels!.map((x) => x)),
+  };
 }
